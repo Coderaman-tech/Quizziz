c72573006b99cf1bfe78504e92aa9d897308576f
Aman Shrivastava <amankumarshrivastava835@gmail.com>
2026-04-20 21:49:24 +0530

    fix-add component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,9 +3,12 @@ import QuizStart from './QuizStart';
 import ProgressBar from './ProgressBar';
 import Timer from './Timer';
 import Questions from './Questions';
+import Results from './Results';
+
 function Quiz() {
   return (
-    <Questions/>
+    <Results/>
+    // <Questions/>
     // <Timer/>
     // <ProgressBar/>
     // <QuizStart/>
